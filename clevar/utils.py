80b0e1640e67a594d148b8acaa44610bb5cbf850
Michel Aguena <aguena@lapp.in2p3.fr>
2021-03-18 19:54:38 +0100

    add funcs: bin_masks none_val

--- clevar/utils.py
+++ clevar/utils.py
@@ -1,3 +1,7 @@
 import numpy as np
 
 veclen = np.vectorize(len)
+def bin_masks(values, bins):
+    return np.array([(values>=b0)*(values<b1) for b0, b1 in zip(bins[:-1], bins[1:])])
+def none_val(value, none_value):
+    return value if value is not None else none_value
