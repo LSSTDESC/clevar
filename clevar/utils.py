e79f21a1dfecc6dbbbcfe6c6c56c67b03a46b69f
Michel Aguena <aguena@lapp.in2p3.fr>
2021-04-01 17:42:22 +0200

    add utils.logbins func

--- clevar/utils.py
+++ clevar/utils.py
@@ -18,3 +18,21 @@ def none_val(value, none_value):
         Value if value is not None else none_value
     """
     return value if value is not None else none_value
+def logbins(values, nbins):
+    """
+    Make log10 spaced bins from data
+
+    Parameters
+    ----------
+    values: array
+        Data
+    nbins: int
+        Number of bins
+
+    Returns
+    -------
+    ndarray
+        Log10 spaced bins based on values
+    """
+    logvals = np.log10(values)
+    return np.logspace(logvals.min(), logvals.max(), nbins)
