051002b11eb974ac80bc3ce1f081c4d4b3cb5535
Michel Aguena <aguena@lapp.in2p3.fr>
2021-04-12 12:27:09 +0200

    improve metrics

--- clevar/__init__.py
+++ clevar/__init__.py
@@ -1,6 +1,6 @@
 """ ClEvaR is a code to validate and evaluate cluster catalogs"""
 from .catalog import ClData, ClCatalog
-from footprint import Footprint
+from .footprint import Footprint
 from . import constants
 from . import geometry
 from . import utils
--- clevar/footprint/__init__.py
+++ clevar/footprint/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import warnings
 
-from ..utils import hp
 from .footprint import Footprint
+from .artificial import create_footprint as create_artificial_footprint
 from . import artificial
