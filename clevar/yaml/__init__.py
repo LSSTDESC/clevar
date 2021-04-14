5e599a033e84ba02cef6f10d402228a60727aad1
Michel Aguena <aguena@lapp.in2p3.fr>
2021-04-14 13:38:21 +0200

    mv exe functionality for yaml module

---
+++ clevar/yaml/__init__.py
@@ -0,0 +1,5 @@
+from .match_metrics_distances import run as match_metrics_distances
+from .match_metrics_mass import run as match_metrics_mass
+from .match_metrics_recovery_rate import run as match_metrics_recovery_rate
+from .match_metrics_redshift import run as match_metrics_redshift
+from .match_proximity import run as match_proximity
