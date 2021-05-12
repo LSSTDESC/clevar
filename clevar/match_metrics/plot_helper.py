f27f2345262ce2f9e00157bc592de5cd087eaf6b
Michel Aguena <aguena@lapp.in2p3.fr>
2021-05-12 17:46:49 +0200

    add rotate arg to plot_hist_line

--- clevar/match_metrics/plot_helper.py
+++ clevar/match_metrics/plot_helper.py
@@ -25,7 +25,7 @@ def add_grid(ax, major_lw=0.5, minor_lw=0.1):
     ax.yaxis.grid(True, which='major', lw=major_lw)
     ax.xaxis.grid(True, which='minor', lw=minor_lw)
     ax.yaxis.grid(True, which='minor', lw=minor_lw)
-def plot_hist_line(hist_values, bins, ax, shape='steps', **kwargs):
+def plot_hist_line(hist_values, bins, ax, shape='steps', rotate=False, **kwargs):
     """
     Plot recovey rate as lines. Can be in steps or continuous
 
@@ -39,6 +39,10 @@ def plot_hist_line(hist_values, bins, ax, shape='steps', **kwargs):
         Ax to add plot
     shape: str
         Shape of the line. Can be steps or line.
+    rotate: bool
+        Invert x-y axes in plot
+    kwargs: parameters
+        Additional parameters for plt.plot
     """
     if shape=='steps':
         data = (np.transpose([bins[:-1], bins[1:]]).flatten(),
@@ -47,6 +51,8 @@ def plot_hist_line(hist_values, bins, ax, shape='steps', **kwargs):
         data = (0.5*(bins[:-1]+bins[1:]), hist_values)
     else:
         raise ValueError(f"shape ({shape}) must be 'steps' or 'line'")
+    if rotate:
+        data = data[::-1]
     ax.plot(*data, **kwargs)
 def get_bin_label(edge_lower, edge_higher,
                   format_func=lambda v:v):
