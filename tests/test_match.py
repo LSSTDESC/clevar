7f22544d2d89db7c0794b9c3e0922a44424eb63c
Michel Aguena <aguena@lapp.in2p3.fr>
2021-06-07 19:39:12 +0200

    add tests for mem matching

--- tests/test_match.py
+++ tests/test_match.py
@@ -4,9 +4,9 @@ import os
 import numpy as np
 from numpy.testing import assert_raises, assert_allclose, assert_equal
 
-from clevar.catalog import ClCatalog
+from clevar.catalog import ClCatalog, MemCatalog
 from clevar.match.parent import Match
-from clevar.match import ProximityMatch
+from clevar.match import ProximityMatch, MembershipMatch
 
 def test_parent():
     mt = Match()
@@ -14,24 +14,20 @@ def test_parent():
     assert_raises(NotImplementedError, mt.multiple, None, None)
     assert_raises(NotImplementedError, mt.match_from_config, None, None, None, None)
 
-def _test_mt_results(mt, c1, c2, multiple_res1, multiple_res2,
-                    unique_res1, unique_res2, cross_res1, cross_res2):
+def _test_mt_results(cat, multi_self, self, cross, multi_other=None, other=None):
+    multi_other = multi_self if multi_other is None else multi_other
+    other = self if other is None else other
     # Check multiple match
     slists = lambda mmt: [sorted(l) for l in mmt]
-    assert_equal(slists(c1['mt_multi_self']), slists(multiple_res1))
-    assert_equal(slists(c1['mt_multi_other']), slists(multiple_res1))
-    assert_equal(slists(c2['mt_multi_self']), slists(multiple_res2))
-    assert_equal(slists(c2['mt_multi_other']), slists(multiple_res2))
+    assert_equal(slists(cat['mt_multi_self']), slists(multi_self))
+    assert_equal(slists(cat['mt_multi_other']), slists(multi_other))
     # Check unique
-    assert_equal(c1['mt_self'], unique_res1)
-    assert_equal(c1['mt_other'], unique_res1)
-    assert_equal(c2['mt_self'], unique_res2)
-    assert_equal(c2['mt_other'], unique_res2)
+    assert_equal(cat['mt_self'], self)
+    assert_equal(cat['mt_other'], other)
     # Check cross
-    assert_equal(c1['mt_cross'], cross_res1)
-    assert_equal(c2['mt_cross'], cross_res2)
+    assert_equal(cat['mt_cross'], cross)
 
-def get_test_data():
+def get_test_data_prox():
     input1 = {
         'id': [f'CL{i}' for i in range(5)],
         'ra': [0., .0001, 0.00011, 25, 20],
@@ -44,7 +40,7 @@ def get_test_data():
     input2['mass'][:3] = input2['mass'][:3][::-1]
     return input1, input2
 def test_proximity(CosmoClass):
-    input1, input2 = get_test_data()
+    input1, input2 = get_test_data_prox()
     c1 = ClCatalog('Cat1', **input1)
     c2 = ClCatalog('Cat2', **input2)
     print(c1.data)
@@ -76,8 +72,8 @@ def test_proximity(CosmoClass):
     mt.unique(c2, c1, 'angular_proximity')
     c1.cross_match()
     c2.cross_match()
-    _test_mt_results(mt, c1, c2, multiple_res1=mmt, unique_res1=smt, cross_res1=smt,
-                     multiple_res2=mmt[:-1], unique_res2=smt[:-1], cross_res2=smt[:-1])
+    _test_mt_results(c1, multi_self=mmt, self=smt, cross=smt)
+    _test_mt_results(c2, multi_self=mmt[:-1], self=smt[:-1], cross=smt[:-1])
     # Check unique with mass preference
     for col in ('mt_self', 'mt_other'):
         c1[col] = None
@@ -87,8 +83,8 @@ def test_proximity(CosmoClass):
     mt.cross_match(c1)
     mt.cross_match(c2)
     smt = ['CL2', 'CL1', 'CL0', 'CL3', None]
-    _test_mt_results(mt, c1, c2, multiple_res1=mmt, unique_res1=smt, cross_res1=smt,
-                     multiple_res2=mmt[:-1], unique_res2=smt[:-1], cross_res2=smt[:-1])
+    _test_mt_results(c1, multi_self=mmt, self=smt, cross=smt)
+    _test_mt_results(c2, multi_self=mmt[:-1], self=smt[:-1], cross=smt[:-1])
     # Check unique with z preference
     for col in ('mt_self', 'mt_other'):
         c1[col] = None
@@ -99,8 +95,8 @@ def test_proximity(CosmoClass):
     mt.cross_match(c1)
     mt.cross_match(c2)
     smt = ['CL1', 'CL0', 'CL2', 'CL3', None]
-    _test_mt_results(mt, c1, c2, multiple_res1=mmt, unique_res1=smt, cross_res1=smt,
-                     multiple_res2=mmt[:-1], unique_res2=smt[:-1], cross_res2=smt[:-1])
+    _test_mt_results(c1, multi_self=mmt, self=smt, cross=smt)
+    _test_mt_results(c2, multi_self=mmt[:-1], self=smt[:-1], cross=smt[:-1])
     # Error for unkown preference
     assert_raises(ValueError, mt.unique, c1, c2, 'unknown')
     # Check save and load matching
@@ -163,7 +159,7 @@ def test_proximity(CosmoClass):
     mt.multiple(c1, c2, radius_selection='other')
     mt.multiple(c1, c2, radius_selection='min')
 def test_proximity_cfg(CosmoClass):
-    input1, input2 = get_test_data()
+    input1, input2 = get_test_data_prox()
     c1 = ClCatalog('Cat1', **input1)
     c2 = ClCatalog('Cat2', **input2)
     print(c1.data)
@@ -199,19 +195,132 @@ def test_proximity_cfg(CosmoClass):
         # Check prep cat
     assert_allclose(c2.mt_input['ang'], np.ones(c2.size)/3600.)
         # Check match
-    _test_mt_results(mt, c1, c2, multiple_res1=mmt, unique_res1=smt, cross_res1=smt,
-                     multiple_res2=mmt[:-1], unique_res2=smt[:-1], cross_res2=smt[:-1])
+    _test_mt_results(c1, multi_self=mmt, self=smt, cross=smt)
+    _test_mt_results(c2, multi_self=mmt[:-1], self=smt[:-1], cross=smt[:-1])
     ### test 1 ###
     mt_config['which_radius'] = 'cat1'
     c1._init_match_vals()
     c2._init_match_vals()
     mt.match_from_config(c1, c2, mt_config, cosmo=cosmo)
-    _test_mt_results(mt, c1, c2, multiple_res1=mmt, unique_res1=smt, cross_res1=smt,
-                     multiple_res2=mmt[:-1], unique_res2=smt[:-1], cross_res2=smt[:-1])
+    _test_mt_results(c1, multi_self=mmt, self=smt, cross=smt)
+    _test_mt_results(c2, multi_self=mmt[:-1], self=smt[:-1], cross=smt[:-1])
     ### test 2 ###
     mt_config['which_radius'] = 'cat2'
     c1._init_match_vals()
     c2._init_match_vals()
     mt.match_from_config(c1, c2, mt_config, cosmo=cosmo)
-    _test_mt_results(mt, c1, c2, multiple_res1=mmt, unique_res1=smt, cross_res1=smt,
-                     multiple_res2=mmt[:-1], unique_res2=smt[:-1], cross_res2=smt[:-1])
+    _test_mt_results(c1, multi_self=mmt, self=smt, cross=smt)
+    _test_mt_results(c2, multi_self=mmt[:-1], self=smt[:-1], cross=smt[:-1])
+def get_test_data_mem():
+    ncl = 5
+    input1 = {
+        'id': [f'CL{i}' for i in range(ncl)],
+        'mass': [30+i for i in range(ncl)],
+    }
+    input2 = {k:v[:-1] for k, v in input1.items()}
+    # members
+    mem_dat = [(f'MEM{imem}', f'CL{icl}') for imem, icl in
+        enumerate([i for i in range(ncl) for j in range(i, ncl)])]
+    input1_mem = {'id_cluster': [f'CL{i}' for i in range(ncl) for j in range(i, ncl)]}
+    input2_mem = {'id_cluster': [f'CL{i}' for i in range(ncl) for j in range(i, ncl)][:-1]}
+    input1_mem['id'] = [f'MEM{i}' for i in range(len(input1_mem['id_cluster']))]
+    input2_mem['id'] = [f'MEM{i}' for i in range(len(input2_mem['id_cluster']))]
+    input1_mem['ra'] = np.arange(len(input1_mem['id_cluster']))
+    input2_mem['ra'] = np.arange(len(input2_mem['id_cluster']))
+    input1_mem['dec'] = np.zeros(len(input1_mem['id_cluster']))
+    input2_mem['dec'] = np.zeros(len(input2_mem['id_cluster']))
+    input2_mem['id_cluster'][0] = f'CL{ncl-2}'
+    c1 = ClCatalog('Cat1', **input1)
+    c2 = ClCatalog('Cat2', **input2)
+    mem1 = MemCatalog('Mem1', **input1_mem)
+    mem2 = MemCatalog('Mem2', **input2_mem)
+    return c1, c2, mem1, mem2
+def test_membership():
+    c1, c2, mem1, mem2 = get_test_data_mem()
+    print(c1.data)
+    print(c2.data)
+    # init match
+    mt = MembershipMatch()
+    # Try to use fill_shared_members before fill_shared_members 
+    assert_raises(ValueError, mt.fill_shared_members, c1, c2, mem1, mem2)
+    # Check both methods
+    mt.match_members(mem1, mem2, method='id')
+    mt2 = MembershipMatch()
+    mt2.match_members(mem1, mem2, method='angular_distance', radius='1arcsec')
+    assert_equal(mt.matched_mems, mt2.matched_mems)
+    # Save and load matched members
+    mt.save_matched_members('temp_mem.txt')
+    mt2.load_matched_members('temp_mem.txt')
+    assert_equal(mt.matched_mems, mt2.matched_mems)
+    os.system('rm temp_mem.txt')
+    # Fill shared members
+    mt.fill_shared_members(c1, c2, mem1, mem2)
+    # Save and load shared members
+    mt.save_shared_members(c1, c2, 'temp')
+    c1_test, c2_test = ClCatalog('test1'), ClCatalog('test2')
+    mt.load_shared_members(c1_test, c2_test, 'temp')
+    assert_equal(c1.mt_input, c1_test.mt_input)
+    assert_equal(c2.mt_input, c2_test.mt_input)
+    os.system('rm temp.1.p temp.2.p')
+    # Check multiple match
+    mt.multiple(c1, c2)
+    mt.multiple(c2, c1)
+    mt.unique(c1, c2, 'shared_member_fraction')
+    mt.unique(c2, c1, 'shared_member_fraction')
+    c1.cross_match()
+    c2.cross_match()
+    print(c1)
+    print(c2)
+    print(c1['mt_multi_self', 'mt_multi_other'])
+    print(c2['mt_multi_self', 'mt_multi_other'])
+    mmt1 = [['CL0', 'CL3'], ['CL1'], ['CL2'], ['CL3'], []]
+    mmt2 = [['CL0'], ['CL1'], ['CL2'], ['CL0', 'CL3'], ]
+    smt = ['CL0', 'CL1', 'CL2', 'CL3', None]
+    _test_mt_results(c1, multi_self=mmt1, self=smt, cross=smt)
+    _test_mt_results(c2, multi_self=mmt2, self=smt[:-1], cross=smt[:-1])
+    # Check with minimum_share_fraction
+    c1._init_match_vals()
+    c2._init_match_vals()
+    mt.multiple(c1, c2)
+    mt.multiple(c2, c1)
+    mt.unique(c1, c2, 'shared_member_fraction', minimum_share_fraction=.7)
+    mt.unique(c2, c1, 'shared_member_fraction', minimum_share_fraction=.7)
+    c1.cross_match()
+    c2.cross_match()
+    print('########')
+    print(c1)
+    print(c2)
+    print(c1['mt_multi_self', 'mt_multi_other'])
+    print(c2['mt_multi_self', 'mt_multi_other'])
+    smt = ['CL0', 'CL1', 'CL2', 'CL3', None]
+    cmt = ['CL0', 'CL1', 'CL2', None, None]
+    _test_mt_results(c1, multi_self=mmt1, self=smt, cross=cmt, other=cmt)
+    _test_mt_results(c2, multi_self=mmt2, self=cmt[:-1], cross=cmt[:-1], other=smt[:-1])
+    # Check with minimum_share_fraction
+    c1._init_match_vals()
+    c2._init_match_vals()
+    mt.multiple(c1, c2)
+    mt.multiple(c2, c1)
+    mt.unique(c1, c2, 'shared_member_fraction', minimum_share_fraction=.9)
+    mt.unique(c2, c1, 'shared_member_fraction', minimum_share_fraction=.9)
+    c1.cross_match()
+    c2.cross_match()
+    print('########')
+    print(c1)
+    print(c2)
+    print(c1['mt_multi_self', 'mt_multi_other'])
+    print(c2['mt_multi_self', 'mt_multi_other'])
+    smt = [None, 'CL1', 'CL2', 'CL3', None]
+    omt = ['CL0', 'CL1', 'CL2', None, None]
+    cmt = [None, 'CL1', 'CL2', None, None]
+    _test_mt_results(c1, multi_self=mmt1, self=smt, cross=cmt, other=omt)
+    _test_mt_results(c2, multi_self=mmt2, self=omt[:-1], cross=cmt[:-1], other=smt[:-1])
+    # Check save and load matching
+    mt.save_matches(c1, c2, out_dir='temp', overwrite=True)
+    c1_test, c2_test = get_test_data_mem()[:2]
+    mt.load_matches(c1_test, c2_test, out_dir='temp')
+    for col in c1.data.colnames:
+        if col[:3]=='mt_':
+            assert_equal(c1[col], c1_test[col])
+            assert_equal(c2[col], c2_test[col])
+    os.system('rm -rf temp')
