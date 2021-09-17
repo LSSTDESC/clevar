3e523ee81fe3fd9bc4d51ac0726cf29030421fa2
Michel Aguena <aguena@lapp.in2p3.fr>
2021-09-17 16:00:28 +0200

    update tests

--- tests/test_footprint.py
+++ tests/test_footprint.py
@@ -31,9 +31,7 @@ def test_footprint():
     ftpt2 = Footprint(nside=nside, pixels=list(set(pixels2)))
     # Footprint functions
     print(ftpt1)
-    ftpt1.get_map(ftpt1['zmax'])
-    assert(1>ftpt1.get_coverfrac(0, 0, 0.1, 1, 'arcsec', cosmo=cosmo))
-    assert(1>ftpt1.get_coverfrac_nfw2D(0, 0, 0.1, 1, 'arcsec', 1, 'arcsec', cosmo=cosmo))
+    ftpt1.get_map('zmax')
     # Load external
     ftpt1.data.write('ftpt1.fits', overwrite=True)
     ftpt1 = Footprint.read('ftpt1.fits', pixel_name='pixel', nside=nside)
