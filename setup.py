d6717918e9b6bc497272ded39ee56627709179e6
Michel Aguena <aguena@lapp.in2p3.fr>
2022-05-30 15:18:09 +0200

    fix np version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python"
         ],
-      install_requires=["astropy>=4.0", "numpy>=1.20", "scipy>=1.4", "healpy"],
+      install_requires=["astropy>=4.0", "numpy==1.22", "scipy>=1.4", "healpy"],
       python_requires='>'+str(required_py_version)
 )
 
