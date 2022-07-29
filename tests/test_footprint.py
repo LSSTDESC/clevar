0e9777c8843c96bfe79433a7ebb9b0166369b8b4
Michel Aguena <aguena@lapp.in2p3.fr>
2022-07-29 18:43:27 +0200

    update tests

--- tests/test_footprint.py
+++ tests/test_footprint.py
@@ -26,9 +26,9 @@ def test_footprint():
     # Gen footprint
     nside = 128
     pixels1 = hp.ang2pix(nside, c1['ra'], c1['dec'], lonlat=True)
-    ftpt1 = Footprint(nside=nside, pixels=list(set(pixels1)))
+    ftpt1 = Footprint(nside=nside, pixel=list(set(pixels1)))
     pixels2 = hp.ang2pix(nside, c2['ra'], c2['dec'], lonlat=True)
-    ftpt2 = Footprint(nside=nside, pixels=list(set(pixels2)))
+    ftpt2 = Footprint(nside=nside, pixel=list(set(pixels2)))
     # Footprint functions
     print(ftpt1)
     ftpt1.get_map('zmax')
@@ -52,7 +52,7 @@ def test_coverfrac():
     for nest in (False, True):
         ft = Footprint(
             nside, nest=nest,
-            pixels=hp.query_disc(
+            pixel=hp.query_disc(
                 nside, vec=hp.ang2vec(0, 0, lonlat=True),
                 radius=np.radians(0.1), nest=nest))
         assert_equal(ft.get_coverfrac(0, 0, 0, 5, 'arcmin'), 1)
@@ -65,7 +65,7 @@ def test_artificial_footprint():
     # baseline footprint
     nside = 128
     pixels1 = hp.ang2pix(nside, c1['ra'], c1['dec'], lonlat=True)
-    ftpt1 = Footprint(nside=nside, pixels=list(set(pixels1)))
+    ftpt1 = Footprint(nside=nside, pixel=list(set(pixels1)))
     # Artificial footprint
     ftpt_test = create_footprint(c1['ra'], c1['dec'], nside=nside)
     assert_equal(sorted(ftpt1['pixel']), sorted(ftpt_test['pixel']))
@@ -82,13 +82,13 @@ def test_plot_footprint():
     nside = 128
     # Plot with 165<ra<195
     pixels1 = hp.ang2pix(nside, c1['ra']+165, c1['dec'], lonlat=True)
-    ftpt1 = Footprint(nside=nside, pixels=list(set(pixels1)))
+    ftpt1 = Footprint(nside=nside, pixel=list(set(pixels1)))
     ftpt1.plot('detfrac', auto_lim=True)
     ftpt1.plot('detfrac', figsize=(3, 3))
     assert_raises(ValueError, ftpt1.plot, 'detfrac', ra_lim=[0,1])
     # Plot with 350<ra<20
     pixels1 = hp.ang2pix(nside, c1['ra']+350, c1['dec'], lonlat=True)
-    ftpt1 = Footprint(nside=nside, pixels=list(set(pixels1)))
+    ftpt1 = Footprint(nside=nside, pixel=list(set(pixels1)))
     ftpt1.plot('detfrac', auto_lim=True)
     # Plot with clusters
     assert_raises(TypeError, ftpt1.plot, 'detfrac', cluster='not a cluster')
