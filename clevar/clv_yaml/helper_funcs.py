6ab5c4a4991004f508c4211e0a71b72e7735b020
m-aguena <michel.aguena@gmail.com>
2023-06-02 00:42:06 +0200

    start pylint clevar/clv_yaml/helper_funcs.py

--- clevar/clv_yaml/helper_funcs.py
+++ clevar/clv_yaml/helper_funcs.py
@@ -1,14 +1,13 @@
 """@file yaml/helper_funcs.py
 Helper functions for command line execution
 """
-import os, sys
+import os
 import yaml
-import argparse
 import numpy as np
 
-from clevar.catalog import ClData, ClCatalog, MemCatalog
+from clevar.catalog import ClCatalog
 from clevar import cosmology
-from clevar.utils import none_val, veclen
+from clevar.utils import veclen
 
 
 ######################################################################
@@ -28,9 +27,8 @@ def read_yaml(filename):
     config: dict
         Dictionary with yaml file info
     """
-    f = open(filename, "r")
-    config = yaml.load(f, Loader=yaml.FullLoader)
-    f.close()
+    with open(filename, "r") as file_handle:
+        config = yaml.load(file_handle, Loader=yaml.FullLoader)
     return config
 
 
@@ -45,9 +43,8 @@ def write_yaml(config, filename):
     filename: str
         Name of yaml file
     """
-    f = open(filename, "w")
-    yaml.dump(config, f)
-    f.close()
+    with open(filename, "w") as file_handle:
+        yaml.dump(config, file_handle)
 
 
 yaml.write = write_yaml
@@ -72,21 +69,21 @@ def add_dicts_diff(dict1, dict2, pref="", diff_lines=None):
     """
     if diff_lines is None:
         diff_lines = []
-    for k in set(k for d in (dict1, dict2) for k in d):
-        if k not in dict1:
-            diff_lines.append((f"{pref}[{k}]", "missing", "present"))
+    for key in set(k for d in (dict1, dict2) for k in d):
+        if key not in dict1:
+            diff_lines.append((f"{pref}[{key}]", "missing", "present"))
             return
-        if k not in dict2:
-            diff_lines.append((f"{pref}[{k}]", "present", "missing"))
+        if key not in dict2:
+            diff_lines.append((f"{pref}[{key}]", "present", "missing"))
             return
-        if dict1[k] != dict2[k]:
-            if isinstance(dict1[k], dict):
-                add_dicts_diff(dict1[k], dict2[k], pref=f"{pref}[{k}]", diff_lines=diff_lines)
+        if dict1[key] != dict2[key]:
+            if isinstance(dict1[key], dict):
+                add_dicts_diff(dict1[key], dict2[key], pref=f"{pref}[{key}]", diff_lines=diff_lines)
             else:
-                diff_lines.append((f"{pref}[{k}]", str(dict1[k]), str(dict2[k])))
+                diff_lines.append((f"{pref}[{key}]", str(dict1[key]), str(dict2[key])))
 
 
-def get_dicts_diff(dict1, dict2, keys=None, header=["Name", "dict1", "dict2"], msg=""):
+def get_dicts_diff(dict1, dict2, keys=None, header=("Name", "dict1", "dict2"), msg=""):
     """
     Get all the differences between dictionaries, accounting for nested dictionaries.
     If there are differences, a table with the information is printed.
@@ -97,7 +94,7 @@ def get_dicts_diff(dict1, dict2, keys=None, header=["Name", "dict1", "dict2"], m
         Dictionaries to be compared
     keys: list, None
         List of keys to be compared. If None, all keys are compared
-    header: str
+    header: tuple
         Header for differences table
     msg: str
         Message printed before the differences
@@ -110,8 +107,10 @@ def get_dicts_diff(dict1, dict2, keys=None, header=["Name", "dict1", "dict2"], m
     diff_lines = [header]
     if keys is None:
         keys = set(list(dict1.keys()) + list(dict2.keys()))
-    for k in keys:
-        add_dicts_diff(dict1.get(k, {}), dict2.get(k, {}), pref=f"[{k}]", diff_lines=diff_lines)
+    for key in keys:
+        add_dicts_diff(
+            dict1.get(key, {}), dict2.get(key, {}), pref=f"[{key}]", diff_lines=diff_lines
+        )
     if len(diff_lines) > 1:
         diff_lines = np.array(diff_lines)
         max_sizes = [max(veclen(l)) for l in diff_lines.T]
@@ -119,8 +118,8 @@ def get_dicts_diff(dict1, dict2, keys=None, header=["Name", "dict1", "dict2"], m
         print(msg)
         print(fmts % tuple(diff_lines[0]))
         print(f'  {"-"*max_sizes[0]}-|-{"-"*max_sizes[1]}-|-{"-"*max_sizes[2]}')
-        for l in diff_lines[1:]:
-            print(fmts % tuple(l))
+        for line in diff_lines[1:]:
+            print(fmts % tuple(line))
     return diff_lines[1:]
 
 
@@ -140,11 +139,11 @@ def deep_update(dict_base, dict_update):
     dict_base: dict
         Updated dictionary (the input dict is also updated)
     """
-    for k, v in dict_update.items():
-        if isinstance(v, dict) and k in dict_base:
-            deep_update(dict_base[k], v)
+    for key, value in dict_update.items():
+        if isinstance(value, dict) and key in dict_base:
+            deep_update(dict_base[key], value)
         else:
-            dict_base[k] = dict_update[k]
+            dict_base[key] = dict_update[key]
     return dict_base
 
 
@@ -181,9 +180,9 @@ def get_input_loop(options_msg, actions):
     while loop:
         action = input(f"\n{options_msg}\n")
         loop = action not in actions
-        prt = print(f"Option {action} not valid. Please choose:") if loop else None
-    f, args, kwargs = actions[action]
-    return f(*args, **kwargs)
+        print(f"Option {action} not valid. Please choose:")
+    func, args, kwargs = actions[action]
+    return func(*args, **kwargs)
 
 
 def loadconf(
@@ -232,8 +231,8 @@ def loadconf(
         log_config = config
     else:
         log_config = yaml.read(log_file)
-        for k in add_new_configs:
-            log_config[k] = log_config.get(k, config[k])
+        for key in add_new_configs:
+            log_config[key] = log_config.get(key, config[key])
         diff_configs = get_dicts_diff(
             log_config,
             config,
@@ -246,12 +245,12 @@ def loadconf(
                 "o": (lambda: True, [], {}),
                 "q": (lambda: None, [], {}),
             }
-            f, args, kwargs = {
+            func, args, kwargs = {
                 "ask": (get_input_loop, ["Overwrite(o) and proceed or Quit(q)?", actions_loop], {}),
                 "orverwrite": actions_loop["o"],
                 "quit": actions_loop["q"],
             }[fail_action]
-            if f(*args, **kwargs) is None:
+            if func(*args, **kwargs) is None:
                 return
     deep_update(log_config, config)
     yaml.write(log_config, log_file)
